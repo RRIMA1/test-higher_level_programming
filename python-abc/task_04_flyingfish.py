5431be3fc77d8c7673e642e8c23eaa897c2a13df
Rami RIMA <rami@Ramis-MacBook-Pro.local>
2025-06-30 01:05:35 +0200

    correction task 4

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -13,6 +13,6 @@ class FlyingFish(Fish,Bird):
     def fly(self):
         print("The flying fish is soaring!")
     def swim(self):
-        print("The flying fish is swimning!")
+        print("The flying fish is swimming!")
     def habitat(self):
         print("The fish lives both in water and the sky")
